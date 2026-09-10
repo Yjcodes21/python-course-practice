d11d47672165fd8861e32c3ae1984d74b665ac5b
Yashjeet Sachan <yashjeetsachan21@gmail.com>
2026-09-10 20:32:48 +0530

    add a new thing

--- class 1/class1.py
+++ class 1/class1.py
@@ -1,5 +1,12 @@
 # this is the class of the string slicing here are the some question for it practice 
 
-a= "yashjeet"
-print(a[4]) 
+# a= "yashjeet"
+# print(a[4]) 
+
+# for reversing a string - 
+
+a = input("hey give your string:-")
+print(a[::-1])
+# here the strt is by default is 0 and stop point is the last index value and the steps is -1 do it started printing from the reverse
+
 
